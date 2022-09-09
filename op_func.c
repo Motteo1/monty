ec13e952d6353910d2c0a08b82ede4de5e407868
Motteo1 <105060159+Motteo1@users.noreply.github.com>
2022-09-09 05:21:29 +0300

    Update op_func.c

--- op_func.c
+++ op_func.c
@@ -1,34 +1,41 @@
-#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./monty.h"
 
 /**
- * get_op_func - function to select correct operation function
- * @token1: 1st bytecode input (opcode)
- * Return: pointer to correct operation function
+ * opcode - function in charge of running builtins
+ * @stack: stack given by main
+ * @str: string to compare
+ * @line_cnt: amount of lines
+ *
+ * Return: nothing
  */
-void (*get_op_func(char *token1))(stack_t **stack, unsigned int line_number)
+void opcode(stack_t **stack, char *str, unsigned int line_cnt)
 {
-	instruction_t instruction_s[] = {
-		{"pop", pop},
-		{"pall", pall},
-		{"pint", pint},
-		{"swap", swap},
-		{"add", _add},
-		{"sub", _sub},
-		{"mul", _mul},
-		{"div", _div},
-		{"mod", _mod},
-		{"nop", nop},
-		{NULL, NULL}
-	};
 	int i = 0;
 
-	while (instruction_s[i].f != NULL)
+	instruction_t op[] = INSTRUCTIONS;
+
+	if (!strcmp(str, "stack"))
 	{
-		if (strcmp(token1, instruction_s[i].opcode) == 0)
+		global.data_struct = 1;
+		return;
+	}
+	if (!strcmp(str, "queue"))
+	{
+		global.data_struct = 0;
+		return;
+	}
+
+	while (op[i].opcode)
+	{
+		if (strcmp(op[i].opcode, str) == 0)
 		{
-			return (instruction_s[i].f);
+			op[i].f(stack, line_cnt);
+			return; /* if we found a match, run the function */
 		}
 		i++;
 	}
-	return (NULL);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
 }
